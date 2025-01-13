9542d6dc4fe66692fa7043287bc1bfe880311410
Rubina Pokhrel <pokhrelrubina2061@gmail.com>
2025-01-13 18:57:22 +0545

    Home Page

--- app/(tabs)/mytrip.jsx
+++ app/(tabs)/mytrip.jsx
@@ -1,10 +1,38 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Colors } from "../../constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import StartNewTripCard from "../../components/MyTrips/StartNewTripCard";
 
 export default function MyTrip() {
+  const [userTrips, setUserTrips] = useState([]);
   return (
-    <View>
-      <Text>Mytrip</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>My Trips</Text>
+        <Ionicons name="add-circle" size={33} color={Colors.blue} />
+      </View>
+      {userTrips?.length === 0 ? <StartNewTripCard /> : null}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 25,
+    paddingTop: 40,
+    backgroundColor: Colors.white,
+    height: "100%",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 25,
+  },
+  headerText: {
+    fontFamily: "outfit-bold",
+    fontSize: 30,
+    color: Colors.black,
+  },
+});
---
+++ components/MyTrips/StartNewTripCard.jsx
@@ -0,0 +1,55 @@
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { Colors } from "../../constants/Colors";
+
+export default function StartNewTripCard() {
+  return (
+    <View style={styles.cardContainer}>
+      <Ionicons name="location-sharp" size={35} color={Colors.blue} />
+      <Text style={styles.cardTitle}>No trips planned yet</Text>
+      <Text style={styles.cardDescription}>
+        Ready for your next adventure? Plan your next trip now and create
+        unforgettable memories!
+      </Text>
+      <TouchableOpacity style={styles.startButton}>
+        <Text style={styles.startButtonText}>Start a New Trip</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    padding: 25,
+    marginTop: 50,
+    alignItems: "center",
+
+    gap: 15,
+  },
+  cardTitle: {
+    fontSize: 26,
+    fontFamily: "outfit-medium",
+    color: Colors.black,
+  },
+  cardDescription: {
+    fontSize: 16,
+    fontFamily: "outfit-regular",
+    color: Colors.gray,
+    textAlign: "center",
+    marginTop: 10,
+  },
+  startButton: {
+    paddingVertical: 12,
+    paddingHorizontal: 35,
+    backgroundColor: Colors.blue,
+    borderRadius: 30,
+    marginTop: 20,
+    alignItems: "center",
+  },
+  startButtonText: {
+    color: Colors.white,
+    fontFamily: "outfit-semibold",
+    fontSize: 16,
+  },
+});
